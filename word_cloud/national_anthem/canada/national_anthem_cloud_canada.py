3104b1be37c2ed583ab1e307dba3c7becb3c1ba3
rajacsp <raja.csp@gmail.com>
2018-09-10 20:40:47 -0400

    core python files added

---
+++ word_cloud/national_anthem/canada/national_anthem_cloud_canada.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# the above line is to avoid 'SyntaxError: Non-UTF-8 code starting with' error
+
+'''
+Created on Aug 30, 2018
+
+Course work: NLP
+
+@author: raja
+
+source:
+    https://github.com/amueller/word_cloud/tree/master/examples
+    
+    lyrics:
+    http://www.metrolyrics.com/canada-anthem-text-lyrics-national-anthem.html
+'''
+
+# Import necessary modules
+from os import path
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+from wordcloud import WordCloud, STOPWORDS
+
+# get data directory
+d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+
+# Read the whole text.
+text = open(path.join(d, 'national_anthem_canada_english.txt')).read()
+
+# read the mask image
+image_base = np.array(Image.open(path.join(d, "canada1.png")))
+
+stopwords = set(STOPWORDS)
+
+wc = WordCloud(background_color="white", max_words=2500, mask=image_base,
+               stopwords=stopwords, contour_width=3, contour_color='steelblue')
+
+# generate word cloud
+wc.generate(text)
+
+# store to file
+#wc.to_file(path.join(d, "canada-cloud.png"))
+
+# show
+plt.imshow(wc, interpolation='bilinear')
+plt.axis("off")
+plt.figure()
+plt.imshow(image_base, cmap=plt.cm.gray, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+
+
